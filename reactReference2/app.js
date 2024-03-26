c6ef2a6c0ba31ab9c63314925ab6c95afae2bd06
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2024-03-26 20:08:49 +0530

    create React DOM using vanilla JS

--- reactReference2/app.js
+++ reactReference2/app.js
@@ -1,3 +1,20 @@
-const head = React.createElement('h1', {id:'h1react'}, 'Hey Im h1 created with React')
+const div = React.createElement('div', { id: 'parent' }, [
+    [
+
+        React.createElement('div', { id: 'childA' }, [
+            React.createElement('h2', { id: 'h1react' }, 'Hey Im h1A created with React'),
+            React.createElement('h2', { id: 'h1react' }, 'Hey Im h2A created with React')
+        ])
+    ],
+    [
+        React.createElement('div', { id: 'childB' }, [
+            React.createElement('h2', { id: 'h1react' }, 'Hey Im h1B created with React'),
+            React.createElement('h2', { id: 'h1react' }, 'Hey Im h2B created with React')
+        ])
+    ],
+])
+
+
+const head = React.createElement('h1', { id: 'h1react' }, 'Hey Im h1 created with React')
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(head)
+root.render([div,head]);
