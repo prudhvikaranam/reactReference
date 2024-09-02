db240261329de49917de02a9c4e28bb8a818a925
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2024-09-02 20:02:10 +0530

    integrating restros service

--- reactReference2/projects/Project-1/UI/components/Body.jsx
+++ reactReference2/projects/Project-1/UI/components/Body.jsx
@@ -1,72 +1,63 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Body = () => {
-  const data = [
-    {
-      hotelname: "Dominos",
-      approxcost: "150",
-      rating: "4.5",
-      location: "HMT Nagar",
-      totalitems: "10",
-      image:
-        "https://img.etimg.com/thumb/width-1600,height-900,imgsize-53568,resizemode-75,msid-104144006/industry/cons-products/food/dominos-pizza-hut-and-other-pizza-chains-cut-rates-as-small-rivals-slice-up-market.jpg",
-      hoteltype: ["Veg", "Non-Veg"]
-    },
-    {
-      hotelname: "Santosh Family Dhaba",
-      approxcost: "220",
-      rating: "4.8",
-      location: "Nacharam",
-      totalitems: "25",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAM_JB8LjGV8cXB9HjO5CZ2HglpzezyvpRpQ&s",
-      hoteltype: ["Veg"]
-    }
-  ];
+  const [restros, setRestros] = useState();
 
-  const addRestroItem = (item) => {
-    console.log(item);
+  const fetchRestros = async () => {
+    const res = await axios.get("http://127.0.0.2:8080/getProducts");
+    console.log(res);
+    setRestros(res.data);
   };
 
-  const deleteRestroItem = (item) => {
-    console.log(item);
-  };
+  useEffect(() => {
+    fetchRestros();
+  }, []);
 
   return (
     <>
       <div className="restro-items-container" id="restro-items-container">
-        {data.map((data,i) => {
-          return (
-            <div className="restro-item" id="restro-item" key={i}>
-              <img src={data.image} />
-              <h3 className="restro-header">{data.hotelname}</h3>
-              <div className="restro-details">
-                <span className="restro-detail">
-                  Approx Cost : {data.approxcost} RS/Person
-                </span>
-                <span className="restro-detail">Rating : {data.rating}</span>
+        {!restros ? (
+          <h1>Loading</h1>
+        ) : (
+          restros.map((data, i) => {
+            return (
+              <div className="restro-item" id="restro-item" key={i}>
+                <img src={data.image} />
+                <h3 className="restro-header">{data.hotelname}</h3>
+                <div className="restro-details">
+                  <span className="restro-detail">
+                    Approx Cost : {data.approxcost} RS/Person
+                  </span>
+                  <span className="restro-detail">Rating : {data.rating}</span>
+                </div>
+                <div className="restro-details">
+                  <span className="restro-detail">
+                    Cuisines : {data.hoteltype.toString()}
+                  </span>
+                  <span className="restro-detail">
+                    Items : {data.totalitems}
+                  </span>
+                </div>
+                <div className="restro-details">
+                  <button
+                    className="restro-btn-add"
+                    onClick={() => addRestroItem(data)}
+                  >
+                    Add +
+                  </button>
+                  <button
+                    className="restro-btn-remove"
+                    onClick={() => deleteRestroItem(data)}
+                  >
+                    Remove -
+                  </button>
+                </div>
               </div>
-              <div className="restro-details">
-                <span className="restro-detail">
-                  Cuisines : {data.hoteltype.toString()}
-                </span>
-                <span className="restro-detail">Items : {data.totalitems}</span>
-              </div>
-              <div className="restro-details">
-                <button
-                  className="restro-btn-add"
-                  onClick={() => addRestroItem(data)}
-                >
-                  Add +
-                </button>
-                <button
-                  className="restro-btn-remove"
-                  onClick={() => deleteRestroItem(data)}
-                >
-                  Remove -
-                </button>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
     </>
   );
